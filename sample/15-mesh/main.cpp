c301230de990db3ad0d15b87ccfd86ca8872f5a6
bodguy <alsrudgns99@gmail.com>
2022-01-09 00:50:49 +0900

    add sample files

---
+++ sample/15-mesh/main.cpp
@@ -0,0 +1,20 @@
+#include "GLaDOS.h"
+
+using namespace GLaDOS;
+
+int main(int argc, char** argv) {
+    {
+        PlatformParams params{1024, 800, "15-mesh", "GLaDOS", false};
+        if (!Platform::getInstance().initialize(params)) {
+            return -1;
+        }
+
+        while (Platform::getInstance().isRunning()) {
+            Platform::getInstance().update();
+        }
+    }
+
+    DestructionManager::getInstance().destroyObjects();
+    dumpMemory();
+    return 0;
+}
